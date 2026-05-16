6bf0ea53ed4938a5bd69798ed3724652932523ee
Christopher <cdeha003@odu.edu>
2026-05-16 17:36:07 -0400

    Added create statements for session specific exercises and sets

--- database/schema.sql
+++ database/schema.sql
@@ -21,20 +21,38 @@ CREATE TABLE muscles (
 
 CREATE TABLE exercise_aliases (
     id SERIAL PRIMARY KEY,
-    exercise_id INTEGER REFERENCES exercises(id),
+    exercise_id INTEGER NOT NULL REFERENCES exercises(id),
     alias_name TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE bodyweight_entries (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id),
-    user_weight NUMERIC(4,1) NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    bodyweight NUMERIC(4,1) NOT NULL,
     recorded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE sessions (
+CREATE TABLE workout_sessions (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id),
+    user_id INTEGER NOT NULL REFERENCES users(id),
     started_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     ended_at TIMESTAMP
+        CHECK (ended_at >= started_at)
+);
+
+CREATE TABLE session_exercises (
+    id SERIAL PRIMARY KEY,
+    session_id INTEGER NOT NULL REFERENCES workout_sessions(id),
+    exercise_id INTEGER NOT NULL REFERENCES exercises(id),
+    exercise_order INTEGER NOT NULL
+);
+
+CREATE TABLE sets (
+    id SERIAL PRIMARY KEY,
+    session_exercise_id INTEGER NOT NULL REFERENCES session_exercises(id),
+    set_order INTEGER NOT NULL,
+    weight NUMERIC(4,1) NOT NULL CHECK (weight > 0),
+    reps INTEGER NOT NULL CHECK (reps > 0),
+    rir INTEGER CHECK (rir BETWEEN 0 AND 10),
+    UNIQUE(session_exercise_id, set_order)
 );
