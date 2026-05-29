e577328f45b181d542faca3b21c2f3413fa4b282
Christopher <cdeha003@odu.edu>
2026-05-28 22:07:14 -0400

    starting to address lack of error handling

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi import HTTPException
+from pydantic import BaseModel
 import database
 
 app = FastAPI()
@@ -8,9 +10,7 @@ app = FastAPI()
 def users():
     users = database.get_all_users()
     
-    return {
-        "users": users
-    }
+    return {"users": users}
     
 # start new session
 @app.post("/sessions")
@@ -26,8 +26,8 @@ def start_session(user_id: int):
 @app.get("/sessions/{session_id}")
 def session(session_id: int): 
     session_data = database.get_session(session_id)
+    if session_data is None:
+        raise HTTPException(status_code=404, detail="Session not found")
     
-    return {
-        "session": session_data
-    }
+    return {"session": session_data}
     
